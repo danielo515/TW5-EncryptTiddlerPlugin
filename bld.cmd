7f6dfd177563ff30d24e65334a36d992b8a3599e
danielo515 <rdanielo@gmail.com>
2015-04-04 22:52:57 +0100

    Stupid commit

--- bld.cmd
+++ bld.cmd
@@ -5,9 +5,4 @@ rem Build encryptwidget for TiddlyWiki5
 tiddlywiki.cmd ^
 	.\ ^
 	--verbose ^
-	--build ^
-
-move /Y output\index.html index.html
-move /Y output\readme.md README.MD
-git add index.html
-git add README.MD
+	--build 
